1587e9b32e278ba507863a21971b44a5b25ac5ea
BradyLin <bradyl3@nycstudents.net>
2025-09-10 10:19:52 -0400

    9-10 work

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,27 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
-    File file = new File("nums");
-    ArrayList a1 = new ArrayList<>();
-    try {
-        Scanner scan = new Scanner(file);
 
+    ArrayList a1 = new ArrayList<Integer>();
+    ArrayList a2 = new ArrayList<Integer>();
+    try {
+
+        File file = new File("src/nums");
+        Scanner scan = new Scanner(file);
+        while (scan.hasNextLine()) {
+            a1.add(scan.nextInt());
+            a2.add(scan.nextInt());
+        }
+        scan.close();
     }
     catch (FileNotFoundException e) {
         e.printStackTrace();
     }
+        System.out.println(a1);
+        System.out.println(a2);
+
+        for (int i = 0; i <a1.size(); i ++) {
+            System.out.print(a1.);
+        }
     }
 }
